74094dd76f12722de9888f67f20ec7eb21675d7f
leandro47 <32754449+leandro47@users.noreply.github.com>
2019-12-20 09:07:18 -0300

    adicionando cabeçario

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- titulo da pagina   -->
+  <title>KM Control</title>
+  <!-- css e js  -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/jquery-3.4.1.slim.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 text-center shadow p-3 mb-5 bg-white rounded" style="background-color: #003666!important">
+     <h1 class="font-weight-lighter" style="color: #fff;">
+         KM Control
+     </h1>
+    </div>
+    </div>
+
+</div>
+</body>
+</html>
