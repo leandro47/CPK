608e557cda7bdc2811a8adbc0c993beb8a5da74d
leandro47 <32754449+leandro47@users.noreply.github.com>
2019-12-26 17:50:43 -0300

    considerações finais

--- index.php
+++ index.php
@@ -161,6 +161,9 @@
                 </div>
                 <hr>
                 <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+                    <h2 class="text-center font-weight-lighter text-center" style="color: #003666">Custo por Pneu</h2>
+                    </div>
                     <div class="col-sm-12 col-md-6 col-lg-6">
                         <div class="container shadow p-3 mb-5 bg-white rounded" style="background-color:rgba(195, 221, 249, 0.5)!important;">
                             <h3 class="text-center font-weight-lighter" style="color: #003666">Custo na concorrencia</h3>
@@ -184,14 +187,14 @@
                                     <label class="input-group-text" style="background-color:#003666; color:white" for="inputGroupSelect01">Selecione quantidade de pneus por veiculo</label>
                                 </div>
                                 <select class="custom-select" id="inputGroupSelect01">
-                                    <option selected>1</option>
+                                    <option value>1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
                                     <option value="4">4</option>
                                     <option value="5">5</option>
                                     <option value="6">6</option>
                                     <option value="7">7</option>
-                                    <option value="8">8</option>
+                                    <option selected="8">8</option>
                                     <option value="9">9</option>
                                     <option value="10">10</option>
                                     <option value="11">11</option>
@@ -224,7 +227,7 @@
                           <div class="col-sm-12 col-md-6 col-lg-6">
                             <div class="input-group mb-3" style="background-color:#003666; color:white">
                                 <div class="input-group-prepend">
-                                    <label class="input-group-text" style="background-color:#003666; color:white" for="inputGroupSelect01">Selecione quantidade de pneus por veiculo</label>
+                                    <label class="input-group-text" style="background-color:#003666; color:white" for="inputGroupSelect01">Selecione quantidade de veiculos</label>
                                 </div>
                                 <select class="custom-select" id="inputGroupSelect01">
                                     <option selected>1</option>
@@ -261,9 +264,72 @@
                                     <option value="32">32</option>
                                     <option value="33">33</option>
                                     <option value="34">34</option>
+                                    <option value="35">35</option>
+                                    <option value="36">36</option>
+                                    <option value="37">37</option>
+                                    <option value="38">38</option>
+                                    <option value="39">39</option>
+                                    <option value="40">40</option>
+                                    <option value="41">41</option>
+                                    <option value="42">42</option>
+                                    <option value="43">43</option>
+                                    <option value="44">44</option>
+                                    <option value="45">45</option>
+                                    <option value="46">46</option>
+                                    <option value="47">47</option>
+                                    <option value="48">48</option>
+                                    <option value="49">49</option>
+                                    <option value="50">50</option>
+                                    <option value="51">51</option>
+                                    <option value="52">52</option>
+                                    <option value="53">53</option>
+                                    <option value="54">54</option>
+                                    <option value="55">55</option>
+                                    <option value="56">56</option>
+                                    <option value="57">57</option>
+                                    <option value="58">58</option>
+                                    <option value="59">59</option>
+                                    <option value="60">60</option>
+                                    <option value="61">61</option>
+                                    <option value="62">62</option>
+                                    <option value="63">63</option>
+                                    <option value="64">64</option>
+                                    <option value="65">65</option>
+                                    <option value="66">66</option>
+                                    <option value="67">67</option>
+                                    <option value="68">68</option>
+                                    <option value="69">69</option>
+                                    <option value="70">70</option>
+                                    <option value="71">71</option>
+                                    <option value="72">72</option>
+                                    <option value="73">73</option>
+                                    <option value="74">74</option>
+                                    <option value="75">75</option>
+                                    <option value="76">76</option>
+                                    <option value="77">77</option>
+                                    <option value="78">78</option>
+                                    <option value="79">79</option>
+                                    <option value="80">80</option>
                                 </select>
                             </div>
                           </div>
+                          
+                    <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+                    <h2 class="text-center font-weight-lighter text-center" style="color: #003666">Custo Total</h2>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-6">
+                        <div class="container shadow p-3 mb-5 bg-white rounded" style="background-color:rgba(195, 221, 249, 0.5)!important;">
+                            <h3 class="text-center font-weight-lighter" style="color: #003666">Custo na concorrencia</h3>
+                            <h2 class="text-center font-weight-light" id="custoconcorrente">R$: 0.00</h2>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-6">
+                        <div class="container shadow p-3 mb-5 bg-white rounded" style="background-color:rgba(195, 221, 249, 0.5)!important;">
+                            <h3 class="text-center font-weight-lighter " style="color: #003666">Custo na Bandag</h3>
+                            <h2 class="text-center font-weight-light" id="custobandag">R$: 0.00</h2>
+                        </div>
+                    </div>
+                
                         </div>
                     </div>
                 </div>
